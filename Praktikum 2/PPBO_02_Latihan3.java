9f2a61ddaca3b892f766a17a1c7ef9a4c2ab134a
Aisyahayya <112526411+Aisyahayya@users.noreply.github.com>
2022-10-08 21:53:48 +0700

    Update PPBO_02_Latihan3.java

--- Praktikum 2/PPBO_02_Latihan3.java
+++ Praktikum 2/PPBO_02_Latihan3.java
@@ -1,14 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
-package com.mycompany.ppbo_02_latihan3;
-
-/**
- *
- * @author HP
- */
 // Nama : Aisyah Hayya Imani
 // NIM  : M0521008
 
