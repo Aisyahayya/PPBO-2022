5899c16388afd85d39e66c334579bbf0b37d01e8
Aisyahayya <112526411+Aisyahayya@users.noreply.github.com>
2022-11-17 08:06:11 +0700

    Create PPBO_05_Soal1.java

---
+++ Praktikum 5/PPBO_05_Soal1.java
@@ -0,0 +1,18 @@
+package com.mycompany.ppbo_05_soal1;
+
+// Nama : Aisyah Hayya Imani
+// NIM  : M0521008
+
+import java.util.Scanner;
+
+public class PPBO_05_Soal1 {
+    public static void main(String args[] ) throws Exception {
+        String s;
+        Scanner input = new Scanner(System.in);
+        s = input.nextLine();
+        input.close();
+        for(int a=0;a < s.length();a+=3){
+            System.out.print(s.charAt(a));
+        }
+    }
+}
